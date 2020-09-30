76a0ecdb708c1744a6fc04d6c432d6775af1c3cf
Andrew Aylett <andrew@aylett.co.uk>
2020-09-30 21:47:07 +0100

    Basic Implementation (#2)
    * Use ts-jest to allow WebStorm to run tests natively
    * Turns out I really do prefer npm over yarn
    * Remembers successes within a minute
    * Assign promises to the correct tick

--- src/index.test.ts
+++ src/index.test.ts
@@ -15,11 +15,127 @@
  *
  */
 
+import 'jest';
 import { SelfThrottle } from './index';
+import { millisecondTicker } from './time';
+import assert from 'assert';
+
+jest.mock('./time');
+const mockMillisecondTicker = millisecondTicker as jest.Mock<
+    ReturnType<typeof millisecondTicker>
+>;
+
+type ms = 'ms';
+const ms = 'ms'; // eslint-disable-line no-redeclare
+type MilliSeconds = [number, ms];
+let nextMockTime: MilliSeconds = [0, ms];
+mockMillisecondTicker.mockImplementation(() => {
+    const result = nextMockTime[0];
+    nextMockTime[0] += 1;
+    return result;
+});
+const seconds = (s: number) => {
+    nextMockTime = [nextMockTime[0] + s * 1000, ms];
+};
+
+const buildPromise = <T>(): [
+    Promise<T>,
+    (v: T | PromiseLike<T> | undefined) => void,
+    (reason?: any) => void,
+] => {
+    let resolve: (v: T | PromiseLike<T> | undefined) => void;
+    let reject: (reason?: any) => void;
+    const promise = new Promise<T>((resolve1, reject1) => {
+        resolve = resolve1;
+        reject = reject1;
+    });
+    // @ts-ignore
+    // noinspection JSUnusedAssignment
+    return [promise, resolve, reject];
+};
 
 describe('Can initialise', () => {
     it('is constructable', () => {
         const instance = new SelfThrottle();
         expect(instance).toBeInstanceOf(SelfThrottle);
+    });
+});
+
+const systemUnderTest = () => new SelfThrottle();
+
+describe('Basic event submission', () => {
+    it('will count successes', () => {
+        const instance = systemUnderTest();
+        instance.recordSuccess();
+        expect(instance).toHaveProperty('successes', 1);
+    });
+
+    it('will allow an attempt', async () => {
+        const instance = systemUnderTest();
+        const result = instance.registerAttempt();
+        expect(result).toBeTruthy();
+    });
+
+    it('will only allow one request in the first second', async () => {
+        const instance = systemUnderTest();
+        const one = instance.registerAttempt();
+        const two = instance.registerAttempt();
+        expect(one).toBeTruthy();
+        expect(two).toBeFalsy();
+    });
+});
+
+describe('Promise Submission', () => {
+    it('will count a successful promise', async () => {
+        const instance = systemUnderTest();
+        const promise = Promise.resolve(true);
+        const result = await instance.registerPromise(promise);
+        expect(await promise).toBeTruthy();
+        expect(result).toBeTruthy();
+        expect(instance).toHaveProperty('successes', 1);
+    });
+
+    it('counts promises against the tick they started in', async () => {
+        const [promise, resolve] = buildPromise<boolean>();
+        const instance = systemUnderTest();
+        const returnedPromise = instance.registerPromise(promise);
+        expect(instance).toHaveProperty('successes', 0);
+        seconds(30);
+        resolve(true);
+        expect(await returnedPromise).toBeTruthy();
+        expect(instance).toHaveProperty('successes', 1);
+        seconds(30);
+        expect(instance).toHaveProperty('successes', 0);
+    });
+});
+
+describe('Time', () => {
+    it('forgets successes after a minute', () => {
+        const instance = systemUnderTest();
+        instance.recordSuccess();
+        seconds(60);
+        expect(instance).toHaveProperty('successes', 0);
+    });
+
+    it('remembers successes less than a minute old', () => {
+        const instance = systemUnderTest();
+        instance.recordSuccess();
+        seconds(30);
+        instance.recordSuccess();
+        seconds(30);
+        expect(instance).toHaveProperty('successes', 1);
+    });
+
+    it('a success in the first tick means two attempts in the second', async () => {
+        const instance = systemUnderTest();
+        assert(instance.registerAttempt());
+        instance.recordSuccess();
+        seconds(1);
+        const one = instance.registerAttempt();
+        const two = instance.registerAttempt();
+        const three = instance.registerAttempt();
+        expect(one).toBeTruthy();
+        expect(two).toBeTruthy();
+        expect(three).toBeFalsy();
     });
 });
--- src/index.ts
+++ src/index.ts
@@ -15,11 +15,85 @@
  *
  */
 
-class Bucket {}
+import { millisecondTicker } from './time';
+
+class Bucket {
+    tick: number;
+    expiry: number;
+    successes: number = 0;
+    attempts: number = 0;
+    limit: number;
+
+    constructor(tick: number, limit: number) {
+        this.tick = tick;
+        this.expiry = tick + 60_000;
+        this.limit = limit;
+    }
+
+    try() {
+        if (this.attempts < this.limit) {
+            this.attempts++;
+            return true;
+        }
+        return false;
+    }
+}
 
 export class SelfThrottle {
-    buckets: Bucket;
+    private buckets: Bucket[];
+    private lastTick: number = 0;
+
     constructor() {
         this.buckets = [];
+    }
+
+    private limitForNextTick() {
+        const computedLimit = Math.ceil(
+            (1.2 * this._successes()) / this.buckets.length,
+        );
+        return Math.max(1, isNaN(computedLimit) ? 1 : computedLimit);
+    }
+
+    maybeTick() {
+        const now = millisecondTicker();
+        const diff = now - this.lastTick;
+        if (diff < 1000 && this.buckets.length > 0) {
+            // No need to tick, we're still in the same second
+            return;
+        }
+        this.lastTick += diff - (diff % 1000);
+        this.buckets.unshift(
+            new Bucket(this.lastTick, this.limitForNextTick()),
+        );
+        this.buckets = this.buckets.filter(
+            bucket => this.lastTick < bucket.expiry,
+        );
+    }
+
+    recordSuccess() {
+        this.maybeTick();
+        this.buckets[0].successes += 1;
+    }
+
+    private _successes() {
+        return this.buckets.reduce((prev, cur) => prev + cur.successes, 0);
+    }
+
+    get successes(): number {
+        this.maybeTick();
+        return this._successes();
+    }
+
+    registerAttempt() {
+        this.maybeTick();
+        return this.buckets[0].try();
+    }
+
+    async registerPromise<T>(promise: Promise<T>): Promise<T> {
+        this.maybeTick();
+        const bucket = this.buckets[0];
+        const result = await promise;
+        bucket.successes++;
+        return result;
     }
 }
